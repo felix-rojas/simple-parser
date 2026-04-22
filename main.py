158a66fbd9f8298be9e0fddf74e7e28a70e5b980
felix-rojas <a01201946@tec.mx>
2026-04-22 10:07:37 -0600

    add a new test case, change main to run all cases without throwing

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-from Lexer import *
-from Parser import *
+from Parser import Parser
 import sys
+import glob
 
 if __name__ == '__main__':
-    parser = Parser("test_cases/good/input01.txt")
-    parser.analize()
+    print("\nTesting good cases (should compile)")
+    for file in glob.glob('test_cases/good/input*.txt'):
+        print(f"Parsing {file}")
+        try:
+            parser = Parser(file)
+            parser.analize()
+            print("-> Successfully parsed")
+        except Exception as e:
+            print(f"-> Error: {e}")
+    
+    print("\nTesting bad cases (should throw error)")
+    for file in glob.glob('test_cases/bad/input*.txt'):
+        print(f"Parsing {file}")
+        try:
+            parser = Parser(file)
+            parser.analize()
+            print("-> This should have failed!)")
+        except Exception as e:
+            print(f"-> Expected Error caught: {e}")
