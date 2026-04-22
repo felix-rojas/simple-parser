15796654ac3dc8f3eeff3250c7664bbc55495699
felix-rojas <a01201946@tec.mx>
2026-04-22 10:35:11 -0600

    add more test cases, bug: trailing comma is not an error?

--- main.py
+++ main.py
@@ -3,22 +3,26 @@ import sys
 import glob
 
 if __name__ == '__main__':
-    print("\nTesting good cases (should compile)")
+    print("---------------------------------------")
+    print("Testing good cases (should compile)")
+    print("---------------------------------------")
     for file in glob.glob('test_cases/good/input*.txt'):
         print(f"Parsing {file}")
         try:
             parser = Parser(file)
             parser.analize()
-            print("-> Successfully parsed")
+            print("-> Successfully parsed\n")
         except Exception as e:
-            print(f"-> Error: {e}")
+            print(f"-> Error: {e}\n")
     
-    print("\nTesting bad cases (should throw error)")
+    print("---------------------------------------")
+    print("Testing bad cases (should throw error)")
+    print("---------------------------------------")
     for file in glob.glob('test_cases/bad/input*.txt'):
         print(f"Parsing {file}")
         try:
             parser = Parser(file)
             parser.analize()
-            print("-> This should have failed!)")
+            print("-> This should have failed!\n")
         except Exception as e:
-            print(f"-> Expected Error caught: {e}")
+            print(f"-> Expected Error caught: {e}\n")
